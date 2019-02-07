ac916f26e000737f1d7ffe7c0f81899cf7552393
Derek Stobbs <dstobbs@googlemail.com>
2019-02-07 11:35:12 +0000

    Exercises on Working with Files
    - Completed the exercises on Working with Files in section 9, lecture 76

---
+++ Exercises/Section 9/Lecture 76/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace Lecture_76
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var path = @"..\..\words.txt";	// up two directories from bin\Debug
+
+			var text = File.ReadAllText(path);
+			var words = text.Split(' ');
+
+			Console.WriteLine("Number of words in '{0}' is {1}", Path.GetFileName(path), words.Length);
+
+			var longestWord = "";
+			foreach (var word in words)
+			{
+				if (word.Length > longestWord.Length)
+				{
+					longestWord = word;
+				}
+			}
+
+			Console.WriteLine("Longest word in '{0}' is {1}", Path.GetFileName(path), longestWord);
+		}
+	}
+}
